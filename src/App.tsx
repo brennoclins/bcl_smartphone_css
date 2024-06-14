6c4f0b80613178fa1354e3db4b23c9ce0256aca2
Brenno - BCL-ST <dev@bcl-st.com.br>
2024-06-14 06:45:12 -0300

    feat: creating smartphone

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
+import styles from './smartphone.module.css'
 export function App() {
   return (
-    <>
-      <h1 className="m-2 flex bg-zinc-500 p-2">BCL</h1>
-    </>
+    <main className={styles.container}>
+      <section className={styles.bclsmartphone}>
+        <div className={styles.backgroundPanels}>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+
+        <div className={styles.phone}>
+          <div className={styles.phoneBack}>
+            <div className={styles.phoneLeftSide}>
+              <div className={styles.phoneAntena}></div>
+              <div className={`${styles.phoneButton} ${styles.top}`}></div>
+              <div className={styles.phoneButton}></div>
+              <div className={`${styles.phoneButton} ${styles.bottom}`}></div>
+              <div className={`${styles.phoneAntena} ${styles.bottom}`}></div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
   )
 }
